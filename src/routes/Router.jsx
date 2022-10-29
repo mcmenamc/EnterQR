910f21f829e47889f6cde6b9610925f958ba43ed
mcmenamc <mcmena636@gmail.com>
2022-10-29 18:37:25 -0500

    github pages setup

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -13,7 +13,7 @@ export const Router = () => {
       <NavBar />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />}  />
         </Routes>
       </BrowserRouter>
       <Footer />
